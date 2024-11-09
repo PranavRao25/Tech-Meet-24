0495403092ecc1e9458e743d35a7bf67c75abc77
Pranav Guruprasad Rao <pranavrao2500@gmail.com>
2024-11-09 23:35:15 +0530

    Added abstract class of Context Agent

--- MCoT/ContextAgent.py
+++ MCoT/ContextAgent.py
@@ -8,14 +8,20 @@ class ContextAgent(ABC):
 
     def __init__(self, vb, model_pair, reranker=None):
         self.vb_list = vb
-        self.q_model = model_pair[0]
-        self.parser = model_pair[1]
+        self.q_model = model_pair[0]  # llm
+        self.parser = model_pair[1]  # parser
         self.cross_model = reranker
 
     @abstractmethod
     def query(self, question)->list[str]:
+        """
+            Retrieves total context for the user query
+        """
         raise NotImplementedError('Implement Query function')
 
     @abstractmethod
     def fetch(self, question)->list[str]:
+        """
+            Retrieves context for a subquery by making a call to the Vector Database
+        """
         raise NotImplementedError('Implement Fetch function')
