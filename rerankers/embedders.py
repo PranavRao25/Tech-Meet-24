eb4e96a1a6d4d65c346532d7e0e96bbf8d06b4d7
LVL_UP <126270067+udithdoddapaneni@users.noreply.github.com>
2024-11-10 21:53:00 +0530

    now this loads BGEM3 too

--- rerankers/embedders.py
+++ rerankers/embedders.py
@@ -1,10 +1,9 @@
 from langchain.embeddings.huggingface import HuggingFaceEmbeddings
-import sentence_transformers
 
 # COLBERT = HuggingFaceEmbeddings(model_name="colbert-ir/colbertv2.0")
 ALL_MPNET = HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
 COLBERT = HuggingFaceEmbeddings(model_name="colbert-ir/colbertv2.0")
-
+BGEM3 = HuggingFaceEmbeddings(model_name="BAAI/bge-m3")
 
 if __name__ == "__main__":
     print(COLBERT.embed_query("hello")[:4])
