1e5d54ab415a6db1de96de663b2e9518002d07c2
Pranav Guruprasad Rao <pranavrao2500@gmail.com>
2024-11-25 22:43:17 +0530

    Added a common class for LanceDBSetup.py

--- UnitTests/LanceDBSetup.py
+++ UnitTests/LanceDBSetup.py
@@ -1,34 +1,67 @@
+from typing import List
 import lancedb
 
-# to connect to a database
-db = lancedb.connect('lancedb/test')
-table_name = 'demo'  # table creation
 
-# open an existing table
-tbl = db.open_table(table_name)
+class TextDatabase:
+    def __init__(self, table_name):
+        self.db = lancedb.connect('lancedb/test')
+        self.table_name = table_name
 
-# data structure
-# add list of key-value pairs / json file contents
-# each dictionary must have a vector key present
-data = [
-    {"vector":v, "metadata":"vec1"}
-    for v in ([1,2,3],[2,3,4],[4,5,6])
-]
+        self.is_created = True
+        try:
+            self.tbl = self.db.open_table(self.table_name)
+        except:
+           self.is_created = False
 
-# to add data into existing table
-tbl.add(data)
+    def upsert(self, data):
+        if isinstance(data, List) and all(isinstance(i, dict) for i in data):
+            if self.is_created:
+                self.tbl.add(data)
+            else:
+                self.db.create_table(self.table_name, data)
+                self.tbl = self.db.open_table(self.table_name)
+                self.is_created = True
+        else:
+            raise ValueError("Incorrect Data Format")
 
-# to create and upsert data into it (the schema is inferred from the data
-table = db.create_table(table_name, data=data)
+    def query(self, request, top_k=3)->List[dict]:
+        return self.tbl.search(request).limit(top_k).to_list()
 
-# to create an empty table, you must specify the schema
+    def delete(self):
+        self.db.drop_table(self.table_name)
 
-# to query a table
-query = "check check"
-response = tbl.search(query).limit(5).to_pandas()  # convert to list/df/any format you want
+    def is_empty(self)->bool:
+        return self.tbl.count_rows() == 0
 
-# to delete a table
-db.drop_table(table_name)
-
-# count size of the db
-size = tbl.count_rows()
+# # to connect to a database
+# db = lancedb.connect('lancedb/test')
+# table_name = 'demo'  # table creation
+#
+# # open an existing table
+# tbl = db.open_table(table_name)
+#
+# # data structure
+# # add list of key-value pairs / json file contents
+# # each dictionary must have a vector key present
+# data = [
+#     {"vector":v, "metadata":"vec1"}
+#     for v in ([1,2,3],[2,3,4],[4,5,6])
+# ]
+#
+# # to add data into existing table
+# tbl.add(data)
+#
+# # to create and upsert data into it (the schema is inferred from the data
+# table = db.create_table(table_name, data=data)
+#
+# # to create an empty table, you must specify the schema
+#
+# # to query a table
+# query = "check check"
+# response = tbl.search(query).limit(5).to_pandas()  # convert to list/df/any format you want
+#
+# # to delete a table
+# db.drop_table(table_name)
+#
+# # count size of the db
+# size = tbl.count_rows()
