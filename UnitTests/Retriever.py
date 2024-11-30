9924859366607b1cedf0fd8dc45fba1d18879a43
VISHNU-SHREERAM <vishnushreerammp04@gmail.com>
2024-11-30 21:24:49 +0530

    LanceDb Retriever for testing CoT

--- UnitTests/Retriever.py
+++ UnitTests/Retriever.py
@@ -3,11 +3,18 @@ from sentence_transformers import SentenceTransformer
 model = SentenceTransformer('all-MiniLM-L6-v2')  # Choose an appropriate model
 
 class Agent:
-    def __init__(self):
+    def __init__(self, query=None):
+        """
+        You can query by Agent().query("question")
+        or
+        You can query by Agent("question")
+        """
         self.model = SentenceTransformer('all-MiniLM-L6-v2')  # Choose an appropriate model
         db_path = "UnitTests\lancedb_folder"
         self.db = lancedb.connect(db_path)
         self.table=self.db.open_table('chunked_text_files')
+        if query is not None:
+            return self.query(query)
     def query(self, query,top_k=5):
         query_embedding = self.model.encode([query])[0]
         results = self.table.search(query_embedding).limit(top_k).to_pandas()
