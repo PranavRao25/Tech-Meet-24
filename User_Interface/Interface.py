33cdbc5b4dfadff80d98edf0b3545ad0c983cbec
udithdoddapaneni <udithdoddapaneni@gmail.com>
2024-12-04 19:02:00 +0530

    now if you have gpu then gpus will run on vram

--- User_Interface/Interface.py
+++ User_Interface/Interface.py
@@ -44,7 +44,7 @@ def vb_prep():
 # Define cached loading functions for each model
 @st.cache_resource
 def load_bge_m3():
-    return BGE_M3(), None
+    return BGE_M3(DEVICE), None
 
 @st.cache_resource
 def load_smol_lm():
@@ -55,7 +55,7 @@ def load_smol_lm():
     
 @st.cache_resource
 def load_colbert():
-    model = colBERT()
+    model = colBERT(DEVICE)
     # model = AutoModel.from_pretrained("colbert-ir/colbertv2.0")
     # tokenizer = AutoTokenizer.from_pretrained("colbert-ir/colbertv2.0")
     return model, None
