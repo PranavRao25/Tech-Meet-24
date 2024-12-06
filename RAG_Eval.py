abe1f8d95dd70730e0ab5c4221b2280077074cb8
Pranav Guruprasad Rao <pranavrao2500@gmail.com>
2024-12-06 11:38:55 +0530

    Basic strucuture of RAG_Eval.py and added dependency in requirements.txt

---
+++ RAG_Eval.py
@@ -0,0 +1,25 @@
+import os
+
+from RAG import RAG
+from tabulate import tabulate
+import json
+import os
+
+# RAG setup
+vb = None
+llm = None
+rag = RAG(vb, llm)
+
+os.environ['OPENAI_API_KEY'] = ''
+
+with open("test_data.json", 'r') as f:
+	data = json.load(f)
+
+list_qa = data['data']
+questions = [qa['question'] for qa in list_qa]
+answers = [qa['answer'] for qa in list_qa]
+contexts = [qa['context'] for qa in list_qa]
+
+results_df = rag.ragas_evaluate(questions, answers)
+
+print(tabulate(results_df))
