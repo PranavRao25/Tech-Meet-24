31517c42fbde021d043d0fe923b06298d7d8f4c2
udithdoddapaneni <udithdoddapaneni@gmail.com>
2024-12-06 13:14:28 +0530

    fixed MOE

--- MOE/llm_query_classifier.py
+++ MOE/llm_query_classifier.py
@@ -73,59 +73,59 @@ class QueryClassifier:
         # Generate prompt
         template= """You are an expert query complexity classifier. 
             Your task is to classify the complexity of the following query into one of four levels:
-            - trivial
-            - simple
-            - intermediate 
-            - complex
+            - ONE
+            - TWO
+            - THREE
+            - FOUR
 
             complexity Classification Guidelines:
-            1. trivial Query:
+            1. ONE:
             - Queries that do not require any reasoning or context.
             - These are basic greetings, conversational inputs, or simple factual questions that are commonly known by many language models.
             - Examples: "What is 2+2?", "Hello, how are you?"
 
-            2. simple Query:
+            2. TWO:
             - Direct, factual questions which requires minimal reasoning or context
             - Examples: "What is the capital of France?"
 
-            3. intermediate Query:
+            3. THREE:
             - Requires some reasoning or multi-step thinking and involves moderate level of analysis
             - Examples: "Explain the main causes of the Industrial Revolution"
 
-            4. complex Query:
+            4. FOUR:
             - Requires in-depth analysis and synthesis
             - Examples: "Analyze the long-term geopolitical implications of climate change", 
 
             Query to Classify: "{query}"
 
-            Please respond with a single word indicating the complexity level of the query:
-            e.g.
-            "complexity": "simple"
-
+            *Please respond with a single word indicating the complexity level of the query*
             """
         prompt = ChatPromptTemplate.from_template(template)
         small_chain = {"query": RunnablePassthrough()} | prompt | self.llm_model #.invoke(prompt.format(question=question))
-        llm_response = small_chain.invoke(query).split("complexity:")[-1].strip()
+        # llm_response = small_chain.invoke(query).split("complexity:")[-1].strip()
+        llm_response = small_chain.invoke(query)
         
         llm_response = llm_response.strip()
-        if "trivial" in llm_response:
+        if "ONE" in llm_response:
             return "trivial"
-        elif "simple" in llm_response:
+        elif "TWO" in llm_response:
             return "simple"
-        elif "intermediate" in llm_response:
+        elif "THREE" in llm_response:
             return "intermediate"
-        elif "complex" in llm_response:
+        elif "FOUR" in llm_response:
             return "complex"
         else:
             print("Error in classification")
             return "simple"
-# def load_mistral():
-#     return HuggingFaceHub(
-#         repo_id="mistralai/Mistral-7B-Instruct-v0.3",
-#         model_kwargs={"temperature": 0.5, "max_length": 64, "max_new_tokens": 512}
-#     )
-# if __name__ == "__main__":
-
-#     classifier = QueryClassifier(load_mistral())
-#     output = classifier.classify("what is the time now?")
-#     print(output)
+if __name__ == "__main__":
+    import toml, os
+    config = toml.load("../config.toml")
+    os.environ["HUGGINGFACEHUB_API_TOKEN"] = config["HF_TOKEN"]
+    def load_mistral(temp=0.1):
+        return HuggingFaceHub(
+            repo_id="mistralai/Mistral-7B-Instruct-v0.3",
+            model_kwargs={"temperature": temp, "max_length": 64, "max_new_tokens": 16, "return_full_text":False}
+        )
+    classifier = QueryClassifier(load_mistral(0.1))
+    output = classifier.classify("Analyze how the geopolitical relations between the United States and China might evolve over the next decade, considering the impact of emerging technologies like AI and quantum computing, recent trade disputes, and the Belt and Road Initiative.")
+    print(output)
